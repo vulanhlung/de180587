cc7b780a885fb2a93f5940f2364a9d158f06a074
vulanhlung <nguyenhuuhoangvu204@gmail.com>
2025-05-17 23:16:10 +0700

    lan 2 commit

---
+++ slot2/exercise_1-5/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import PeopleTable from "./PeopleTable";
+
+function App() {
+  return (
+    <div>
+      <h1 className="h1Style">Hello world!</h1>
+      <PeopleTable />
+    </div>
+  );
+}
+
+export default App;
